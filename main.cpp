7d563d7bc8362a1d5382a80b44c4875b67c6a740
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-12-12 15:06:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
+float x=0;
 int main(){
+  int d=0; cin >> d; float numero [d];
+    for (int i=0; i<d; i++){
+        cin >> numero[i];
+    }
+    for (int i=1; i<d; i++){
+        float n=maggiore(numero[i], numero [i-1]);
+        if(n>x) {
+            x=n;
+        }
+    }
+    cout << x;
   return 0;
 }
